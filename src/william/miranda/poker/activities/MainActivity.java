1f66e7e73b8b41d38c6d5fdf709923e00f0c2f8d
william.miranda <william.miranda@lge.com>
2013-12-11 19:10:05 -0200

    Creating table layout

--- src/william/miranda/poker/activities/MainActivity.java
+++ src/william/miranda/poker/activities/MainActivity.java
@@ -1,16 +1,9 @@
 package william.miranda.poker.activities;
 
 import william.miranda.poker.R;
-import william.miranda.poker.model.Carta;
-import william.miranda.poker.model.Carta.Naipe;
 import william.miranda.poker.view.ViewUtils;
 import android.app.Activity;
-import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.ScrollView;
-import android.widget.TextView;
 
 public class MainActivity extends Activity
 {
@@ -28,30 +21,6 @@ public class MainActivity extends Activity
 		//para cada rodada, inicializaremos os atributos de PokerGame
 		//pokerGame.prepararNovaRodada();
 		
-		
-		//cria o corpo do layout
-		ScrollView sv = new ScrollView(this);
-		LinearLayout ll = new LinearLayout(this);
-		ll.setOrientation(LinearLayout.VERTICAL);
-		
-		//cria um ImageView com um drawable
-		TextView tv = new TextView(this);
-		tv.setText("Texto de teste!!!");
-		
-		ImageView iv1 = new ImageView(this);
-		Bitmap bmp = ViewUtils.getBitmap(new Carta(5, Naipe.COPAS));
-		iv1.setImageBitmap(bmp);
-		iv1.setScaleX(2);
-		iv1.setScaleY(2);
-		
-		ImageView iv2 = new ImageView(this);
-		iv2.setImageResource(R.drawable.ic_launcher);
-		
-		sv.addView(ll);
-		ll.addView(tv);
-		ll.addView(iv1);
-		ll.addView(iv2);
-				
-		setContentView(sv);
+		setContentView(R.layout.layout_mesa);
 	}
 }
