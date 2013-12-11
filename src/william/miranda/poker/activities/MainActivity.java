c15e96d48d4991808f256f6c1b2fd9d0e47289a2
william.miranda <william.miranda@lge.com>
2013-12-11 16:30:55 -0200

    View Tests

--- src/william/miranda/poker/activities/MainActivity.java
+++ src/william/miranda/poker/activities/MainActivity.java
@@ -1,7 +1,16 @@
 package william.miranda.poker.activities;
 
+import william.miranda.poker.R;
+import william.miranda.poker.model.Carta;
+import william.miranda.poker.model.Carta.Naipe;
+import william.miranda.poker.view.ViewUtils;
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 public class MainActivity extends Activity
 {
@@ -9,5 +18,40 @@ public class MainActivity extends Activity
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
+		
+		/* Armazena o Contexto estaticamente */
+		ViewUtils.context = this;
+		
+		//teremos um Singleton da classe poker game
+		//PokerGame pokerGame = PokerGame.getInstance();
+		
+		//para cada rodada, inicializaremos os atributos de PokerGame
+		//pokerGame.prepararNovaRodada();
+		
+		
+		//cria o corpo do layout
+		ScrollView sv = new ScrollView(this);
+		LinearLayout ll = new LinearLayout(this);
+		ll.setOrientation(LinearLayout.VERTICAL);
+		
+		//cria um ImageView com um drawable
+		TextView tv = new TextView(this);
+		tv.setText("Texto de teste!!!");
+		
+		ImageView iv1 = new ImageView(this);
+		Bitmap bmp = ViewUtils.getBitmap(new Carta(5, Naipe.COPAS));
+		iv1.setImageBitmap(bmp);
+		iv1.setScaleX(2);
+		iv1.setScaleY(2);
+		
+		ImageView iv2 = new ImageView(this);
+		iv2.setImageResource(R.drawable.ic_launcher);
+		
+		sv.addView(ll);
+		ll.addView(tv);
+		ll.addView(iv1);
+		ll.addView(iv2);
+				
+		setContentView(sv);
 	}
 }
--- src/william/miranda/poker/view/ViewUtils.java
+++ src/william/miranda/poker/view/ViewUtils.java
@@ -1,12 +1,22 @@
 package william.miranda.poker.view;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import william.miranda.poker.model.Carta;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 
 public class ViewUtils
 {
 	/* constantes */
 	public static final String FOLDER_CARTAS = "cartas/";
 	public static final String FOLDER_FICHAS = "chips/";
+	
+	/* Contexto */
+	public static Context context;
 	
 	public static String getResourceName(Carta carta)
     {
@@ -36,6 +46,44 @@ public class ViewUtils
     	
     	return sb.toString();
     }
+	
+	public static Drawable getDrawable(Carta carta)
+	{
+        try
+        {
+        	//obtem o nome do asset
+        	String res = getResourceName(carta);
+        	
+        	//abre o stream
+        	InputStream ims = context.getAssets().open(res);
+        	
+        	//carrega o stream como um Drawable
+        	return Drawable.createFromStream(ims, null);
+        }
+        catch(IOException ex)
+        {
+            return null;
+        }
+	}
+	
+	public static Bitmap getBitmap(Carta carta)
+	{
+        try
+        {
+        	//obtem o nome do asset
+        	String res = getResourceName(carta);
+        	
+        	//abre o stream
+        	InputStream ims = context.getAssets().open(res);
+        	
+        	//carrega o stream como um Bitmap
+        	return BitmapFactory.decodeStream(ims);
+        }
+        catch(IOException ex)
+        {
+            return null;
+        }
+	}
 	
 	public static String getDealerButton()
 	{
